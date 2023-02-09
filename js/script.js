e9188c70b297b51989a6c1b9fa022454bc371e26
Vianney Baskinas <vianbask07@yahoo.com>
2023-02-09 01:22:00 -0800

    Assignment 1 submit

---
+++ js/script.js
@@ -0,0 +1,131 @@
+//STEP 1 Camel Case
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+//STEP 2 Literal Expressions
+// numeric literal expression
+// 5.0
+// string literal expression
+// 'Good Morning'
+// Boolean literal expression
+// false
+// null literal expression
+// null
+
+//STEP 3 two examples of complex / variable expressions.
+// numInput = prompt('Enter the number of people in your household: ')
+// x = 0
+
+// STEP 4 Declare (but do not assign) 9 variables for the following: 
+// let firstName
+// let lastName
+// let strAddress
+// let strCity
+// let strState
+// let zipCode
+// let intYourage
+// let referralSource
+// let mayWeContactYou
+
+// STEP 5 3 ways for declaring and assigning values to those  3 variables.
+// let first_name
+// firstName = prompt("Enter your first name: ")
+
+// let zipCode = '92154'
+
+// let strCity = 'San Diego', zipCode = '92154'
+
+// STEP 6 Add a number and a string and display in the browser's console window.
+// concat = 'String'+ 100
+// console.log(concat)
+
+// STEP 7 Add a Boolean and a string; add a number and a Boolean. Display in console.
+// x = true + 'blue'
+// console.log(x)
+
+// y = true + 5
+// console.log(y)
+
+// STEP 8
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+// console.log(someString)
+
+// STEP 9
+// Create a variable that produces a null value in the console window.
+// Then, create a variable that produces an undefined value in the console window.
+// g = null 
+// console.log(typeof(g),g)
+
+// let y
+// c = y
+// console.log(c)
+
+// STEP 10 Use the unary typeof operator on various literals 
+//  x = 'Good Afternoon'
+//  y = 135
+//  z = false
+//  v = null
+//  w = undefined
+//  console.log(x, typeof(x))
+//  console.log(y, typeof(y))
+//  console.log(z, typeof(z))
+//  console.log(v, typeof(v))
+//  console.log(w, typeof(w))
+
+// STEP 11 
+// alert("Hello "+ "Vianney Baskinas"+", welcome to the Javascript class!")
+
+// STEP 12 
+// let name = 'Vianney Baskinas'
+// alert("Hello " + name + ", welcome to the Javascript class!")
+
+// STEP 13 
+// let name = 'Vianney Baskinas'
+// let course = "Javascript"
+// alert("Hello " + name + ", welcome to the " +course +" class!")
+
+// STEP 14
+// let name = 'Vianney Baskinas'
+// let course = "Javascript"
+// alert("Hello " + name + ".\nWelcome to the " +course +" class!")
+
+// STEP 15
+// let name = prompt("Enter your name: ")
+// let course = "Javascript"
+// alert("Hello " + name + ".\nWelcome to the " +course +" class!")
+
+// STEP 16
+// let name = prompt("Enter your name: ")
+// let course = prompt("Enter your course: ")
+// alert("Hello " + name + ".\nWelcome to the " +course +" class!")
+
+// STEP 17
+// let y = 20
+// console.log("Sum of x and y is : " + (x + y))
+
+// STEP 18
+// let x = 20
+// x += 20
+// console.log(x)
+
+// STEP 19
+// let x = 20
+// x *= 5
+// console.log(x)
+
+// STEP 20
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
+
+// STEP 21
+// let x = 10, y = 5, z = 7
+// console.log(x > y && y < z) 
+
+// STEP 22
+// let x, y, z
+// if (x == y || y == z)
+//     console.log(x != z)
